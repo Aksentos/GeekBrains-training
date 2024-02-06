5e5e24f886ed37d82cb311617d2966b003edd885
Aksentos <108864454+Aksentos@users.noreply.github.com>
2024-02-06 14:13:01 +0300

    Update Крестики_нолики.py

--- Additional tasks/Крестики_нолики.py
+++ Additional tasks/Крестики_нолики.py
@@ -1,74 +1,91 @@
-"""
-Написать игру крестики нолики для игры с пк
-"""
+"""Крестики нолики"""
+# Пока игра расчитана на 2 игроков без ИИ.
+game_field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Игровое поле
 
-import tkinter as tk
+player_1 = "x"
+player_2 = "o"
+steps = []  # список ходов
+win = 0
 
+# Какие поля должны быть заполнены для победы
+win_conditinons = [
+    [1, 2, 3],
+    [3, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [7, 5, 3],
+    [1, 3, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+]
 
-window = tk.Tk()  # открывает окно приложения
-window.title("Крестики vs. нолики")
+# Проверка на победу
+def wins(player):
+    global win
+    for item in win_conditinons:
+        if item.count(player) == 3: # если в списке все элементы "х" или "о"
+            print(f"\nВыиграл {player}\n")
+            win = True
+            return win
+    return win
 
+# Заполняем победные списки элементами
+def win_cond(num, player):
+    for i in range(len(win_conditinons)):
+        for j in range(len(win_conditinons[i])):
+            if num == win_conditinons[i][j]:
+                win_conditinons[i][j] = player
 
-# кнопка закрывает окно приложения
-def button_quit(frame):
-    quit = tk.Button(
-        master=frame,
-        text="quit",
-        relief=tk.RAISED,
-        borderwidth=3,
-        command=window.destroy,
-    )
-    return quit
+# Вывод текущего игрового поля
+def field(lsit):
+    print("*" * 13)
+    for i in lsit:
+        print(f"* {i}", end=" *\n")
+    print("*" * 13)
 
+# ход игры
+def game(player, list):
+    field(list)  # Вывод текущего игрового поля
+    step = input(f"Куда ставим {player}?: ")  # спрашиваем в какую ячейку ставим знак
+    try:  # проверка ввода
+        step = int(step) 
+    except:
+        print("\nНеверный ввод\n")
+        game(player, list)
+    if step not in range(1, 10):  # проверка ввода
+        print("\nНеверный ввод, выберите другое место\n")
+        game(player, list)
+    elif step in steps:  # проверка ввода
+        print("\nЭто поле уже занято, выберите другое место\n")
+        game(player, list)
+    elif step == 1:
+        game_field[0][0] = player
+    elif step == 2:
+        game_field[0][1] = player
+    elif step == 3:
+        game_field[0][2] = player
+    elif step == 4:
+        game_field[1][0] = player
+    elif step == 5:
+        game_field[1][1] = player
+    elif step == 6:
+        game_field[1][2] = player
+    elif step == 7:
+        game_field[2][0] = player
+    elif step == 8:
+        game_field[2][1] = player
+    elif step == 9:
+        game_field[2][2] = player
 
-# кнопка Х,О
-def button_x(frame):
-    btn = tk.Button(
-        master=frame,
-        text=" ",
-        width=10,
-        height=5,
-        bg="blue",
-        fg="yellow",
-        # смена кнопки (пока не работает)
-    )
-    return btn
+    steps.append(step)  # обновляем список ходов
+    win_cond(step, player)  # обновляем победные списки
+    
 
-
-# смена кнопки (пока не работает)
-def change_button(self):
-    if self["text"] == " ":
-        self["text"] = "X"
-    elif self["text"] == "X":
-        self["text"] = "O"
-    elif self["text"] == "O":
-        self["text"] = " "
-
-
-# кнопка сброса (функции еще нет)
-def button_reset(frame):
-    reset = tk.Button(master=frame, text="reset", relief=tk.RAISED, borderwidth=3)
-    return reset
-
-
-# заполнение игрового поля кнопками
-for i in range(4):
-    window.columnconfigure(i, weight=1, minsize=75)
-    window.rowconfigure(i, weight=1, minsize=75)
-    for j in range(3):
-        frame_field = tk.Frame(master=window, relief=tk.RAISED, borderwidth=1)
-        frame_field.grid(row=i, column=j, padx=3, pady=3)
-        if i < 3:
-            btn = button_x(frame_field)  # добавляем кнопки для Х О
-            btn.configure(command=change_button(self=btn)) ## НЕ РАБОТАЕТ!!!!
-            btn.pack()
-        if i == 3:
-            if j == 2:
-                button_quit(frame_field).pack()  # добавляем кнопку выхода из приложения
-            elif j == 1:
-                button_reset(frame_field).pack()  # добавляем кнопку сброса игры
-            else:
-                pass  # добавляем еще какую-нибудь кнопку слева снизу
-
-
-window.mainloop()  # указывает Python, что нужно запустить цикл событий Tkinter
+# Запускаем игру
+while win == False:
+    game(player_1, game_field)
+    wins(player_1)  # проверка на победу
+    if win == True:
+        break
+    game(player_2, game_field)
+    wins(player_2)  # проверка на победу
